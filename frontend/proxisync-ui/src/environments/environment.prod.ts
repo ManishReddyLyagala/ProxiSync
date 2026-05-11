4f949f1c063714dd16ea4825c61874c4e349fd93
Manish-Reddy-007 <manishlyagala2003@gmail.com>
2026-05-11 22:19:20 +0530

    Added production frontend environment

--- frontend/proxisync-ui/src/environments/environment.prod.ts
+++ frontend/proxisync-ui/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  gatewayUrl: 'https://placeholder-gateway-url.com',
-  chatUrl: 'https://placeholder-chat-url.com',
-  callUrl: 'https://placeholder-call-url.com'
+  gatewayUrl: 'https://proxysync-gateway.onrender.com',
+  chatUrl: 'https://proxysync-chatservice.onrender.com',
+  callUrl: 'https://proxisync-callservice.onrender.com'
 };
